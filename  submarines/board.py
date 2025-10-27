39ce81c2c1d6b5e9dcd1b24c61357fbcdfcd5b96
Idan <idansano10@gmail.com>
2025-10-27 15:24:43 +0200

    added create_bool_matrix function

---  submarines/board.py
+++  submarines/board.py
@@ -1,7 +1,10 @@
 def create_matrix(size: int, fill: int = 0) -> list[list[int]]:
-    board_matrix = [[0 for _ in range(size)] for _ in range(size)]
+    board_matrix = [[fill for _ in range(size)] for _ in range(size)]
     return board_matrix
 
+def create_bool_matrix(size: int, fill: bool = False) -> list[list[bool]]:
+    bool_matrix = [[fill for _ in range(size)] for _ in range(size)]
+    return bool_matrix
 
 
 
