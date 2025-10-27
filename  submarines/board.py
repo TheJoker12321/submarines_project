e29993a788d6aab452001083d8aa36a22da1fd83
Idan <idansano10@gmail.com>
2025-10-27 17:03:26 +0200

    added count_remaining_ships function

---  submarines/board.py
+++  submarines/board.py
@@ -6,5 +6,20 @@ def create_bool_matrix(size: int, fill: bool = False) -> list[list[bool]]:
     bool_matrix = [[fill for _ in range(size)] for _ in range(size)]
     return bool_matrix
 
+def in_bounds(size: int, x: int, y: int) -> bool:
+    return 0 < x < size and 0 < y < size
+
+def count_remaining_ships(ships: list[list[int]], shots: list[list[bool]]) -> int:
+    count_remaining = 0
+    for i, j in enumerate(ships):
+        for k, v in enumerate(j):
+            if v == 1:
+                if not shots[i][k]:
+                    count_remaining += 1
+    return count_remaining
+
+
+
+
 
 
